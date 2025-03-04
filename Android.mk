4906674ffa11af75c67a315ddb63424ab34ffa81
lolipuru <banstellazz@gmail.com>
2025-03-04 11:57:28 +0000

    sm8550-common: initial commonized
    Change-Id: I6eb2d1d576b94c1d5ce1a9ccb2695154497618b0
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,28 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),socrates)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,3736c13e4cd9f1bd5f6805e58af69ab7c7b4267a)
-$(call add-radio-file-sha1-checked,radio/aop.img,fc4809ced543846283b57194c7910546b598a589)
-$(call add-radio-file-sha1-checked,radio/aop_config.img,a89c115580cf33b4dff255eece22aea5a7825624)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,0e3cb8da860a46ed8a1b8ce90170696a3fd976fa)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,1c3d663c8aab61aa56859ad3e03d6f371098e046)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,1473931048885464e94cd5735efa8bc58fb131f6)
-$(call add-radio-file-sha1-checked,radio/dsp.img,10bcadcefd2eaf6d87d001d16764054545fa143e)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,a460ca6a9cb8f7b26ae4fcda05bb4643c688fab0)
-$(call add-radio-file-sha1-checked,radio/hyp.img,a1a5de1ff03605279da1fd74c9492a63d4eb81ae)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,cadbd8052eb20823279d37259d12886d1e9f35e5)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,eb7a2b87ec5cf213d6178fd7a9c8df25bb5ef4ff)
-$(call add-radio-file-sha1-checked,radio/modem.img,821e892c5bc42cfc16031d1ecc3a4ccc01504a43)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,662fecd0f5bee43606c053689fe30ec5841031cd)
-$(call add-radio-file-sha1-checked,radio/shrm.img,ede8997b4651f7331d94d8c15ad60a38ade7e460)
-$(call add-radio-file-sha1-checked,radio/tz.img,7febfe8fc86a292dbada4041f471f47526437e2c)
-$(call add-radio-file-sha1-checked,radio/uefi.img,845768bce973f0789297af03fe49fbd72515583d)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,b1bd06b2d45aaf5a69b50dc82269dba9727c4850)
-$(call add-radio-file-sha1-checked,radio/xbl.img,edc079941b4adc84539d6c3669e8075821d4174d)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,6ec62f4a3a8ced71dfce7cd237376aa6b8a344c8)
-$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,4fee406389d2b2b3ae19f21df01bc31b2a070ce0)
-
-endif
